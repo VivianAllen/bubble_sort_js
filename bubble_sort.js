d09a3d03a0fa02cb5b674da482727a382847aaf9
Vivian Allen <vivian.allen@methods.co.uk>
2019-11-27 14:49:44 +0000

    implement ascending sorting

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,5 +1,12 @@
-function bubbleSort(ary) {
-    // code here
+function bubbleSort(ary, order) {
+    for (i=0; i < ary.length; i++) {
+        for (j=0; j <= i; j++) {
+            if ( ary[j] > ary[j+1] ) {
+                [ary[j], ary[j+1]]  = [ary[j+1], ary[j]];
+            };
+        };
+    };
+    return ary;
 };
 
 module.exports = bubbleSort;
