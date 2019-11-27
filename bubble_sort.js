88ef2f05e32553e7b70d216931c80e60154245ae
Vivian Allen <vivian.allen@methods.co.uk>
2019-11-27 15:01:01 +0000

    implement descending sort, add tests for pre-sorted and unsortable arrays

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,12 +1,22 @@
 function bubbleSort(ary, order) {
+
+    function comp(x, y, order) {
+        if (order=='asc') {
+            return x > y;
+        } else {
+            return x < y;
+        };
+    };
+
     for (i=0; i < ary.length; i++) {
         for (j=0; j <= i; j++) {
-            if ( ary[j] > ary[j+1] ) {
+            if (comp(ary[j], ary[j+1], order)) {
                 [ary[j], ary[j+1]]  = [ary[j+1], ary[j]];
             };
         };
     };
     return ary;
+
 };
 
 module.exports = bubbleSort;
--- test_bubble_sort.test.js
+++ test_bubble_sort.test.js
@@ -1,16 +1,49 @@
 const bubbleSort = require('./bubble_sort.js');
 
-test("Test bubbleSort([3, 2, 1], 'asc') = [1, 2, 3]", () => {
+
+test("Test bubbleSort ascending sort", () => {
     input = [3, 2, 1];
     expected = [1, 2, 3];
     received = bubbleSort(input, 'asc');
     expect(received).toEqual(expected);
 });
 
-test("Test bubbleSort([1, 2, 3], 'desc') = [3, 2, 1]", () => {
+
+test("Test bubbleSort descending sort", () => {
     input = [1, 2, 3];
     expected = [3, 2, 1];
     received = bubbleSort(input, 'desc');
     expect(received).toEqual(expected);
 });
 
+
+test("Test bubbleSort already sorted, ascending", () => {
+    input = [1, 2, 3];
+    expected = [1, 2, 3];
+    received = bubbleSort(input, 'asc');
+    expect(received).toEqual(expected);
+});
+
+
+test("Test bubbleSort already sorted, descending", () => {
+    input = [3, 2, 1];
+    expected = [3, 2, 1];
+    received = bubbleSort(input, 'desc');
+    expect(received).toEqual(expected);
+});
+
+
+test("Test bubbleSort cannot be sorted, ascending", () => {
+    input = [1, 1, 1];
+    expected = [1, 1, 1];
+    received = bubbleSort(input, 'asc');
+    expect(received).toEqual(expected);
+});
+
+
+test("Test bubbleSort cannot be sorted, descending", () => {
+    input = [1, 1, 1];
+    expected = [1, 1, 1];
+    received = bubbleSort(input, 'desc');
+    expect(received).toEqual(expected);
+});
